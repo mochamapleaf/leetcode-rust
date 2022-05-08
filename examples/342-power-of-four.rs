5cb276186d1b338d627cf11b1f2ceedf08fdca46
Jim Ye <x59ye@uwaterloo.ca>
2022-05-07 21:06:36 -0400

    better solution for 342

---
+++ examples/342-power-of-four.rs
@@ -0,0 +1,21 @@
+//modules
+
+//struct define
+struct Solution;
+//code
+impl Solution {
+    pub fn is_power_of_four(n: i32) -> bool {
+        n.count_ones() == 1 && n.trailing_zeros() % 2 == 0
+        //more optimized version
+        //n.count ones() == 1 && (n & 0x55555555) != 0
+        //more jin bao version
+        //(n > 0) && (n & 0x2aaaaaaa == 0) && (n & -n) == n
+    }
+}
+
+fn main() {}
+
+#[test]
+fn test_solution() {
+    main();
+}
