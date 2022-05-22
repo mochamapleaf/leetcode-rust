e1b87423e70bf2d6eda829f00f2596f844ee7a81
Xinyu Ye <xinyuye2018@outlook.com>
2022-05-22 09:14:03 -0400

    update

--- examples/342-power-of-four.rs
+++ examples/342-power-of-four.rs
@@ -8,7 +8,7 @@ impl Solution {
         n.count_ones() == 1 && n.trailing_zeros() % 2 == 0
         //more optimized version
         //n.count ones() == 1 && (n & 0x55555555) != 0
-        //more jin bao version
+        //more optimized version
         //(n > 0) && (n & 0x2aaaaaaa == 0) && (n & -n) == n
     }
 }
--- examples/398-random-pick-index.rs
+++ examples/398-random-pick-index.rs
@@ -6,7 +6,7 @@ struct Solution_original {
 impl Solution_original {
     //My original solution
     fn new(nums: Vec<i32>) -> Self {
-        Solution {
+        Solution_original {
             list: nums,
             rng: rand::thread_rng(),
         }
@@ -25,7 +25,91 @@ impl Solution_original {
     }
 }
 
-fn main() {}
+//use rand::Rng;
+use rand::seq::SliceRandom;
+use rand::SeedableRng;
+use rand::rngs::SmallRng;
+
+struct Solution_Hash {
+    my_map: std::collections::HashMap<i32, Vec<usize>>,
+    rng: rand::rngs::SmallRng,
+    //rng: rand::rngs::ThreadRng,
+}
+impl Solution_Hash {
+    fn new(nums: Vec<i32>) -> Self {
+        let mut tmp = std::collections::HashMap::new();
+        for i in 0..nums.len(){
+            let mem_list = tmp.entry(nums[i]).or_insert(Vec::<usize>::new());
+            (*mem_list).push(i);
+        }
+        Solution_Hash {
+            my_map: tmp,
+            rng: SmallRng::from_entropy(),
+            //rng: rand::thread_rng(),
+        }
+    }
+    fn pick(&mut self, target: i32) -> i32 {
+        *self.my_map.get(&target).unwrap().choose(&mut self.rng).unwrap() as i32
+        //let choices = self.my_map.get(&target).unwrap();
+        //if choices.len() ==  1{ return choices[0] as i32;}
+        //choices[self.rng.gen_range(0,choices.len())] as i32
+    }
+}
+
+//use rand::Rng;
+use rand::seq::IteratorRandom;
+struct Solution_oneline_iter {
+    list: Vec<i32>,
+    rng: rand::rngs::ThreadRng,
+}
+impl Solution_oneline_iter {
+    //My original solution
+    fn new(nums: Vec<i32>) -> Self {
+        Solution_oneline_iter {
+            list: nums,
+            rng: rand::thread_rng(),
+        }
+    }
+    fn pick(&mut self, target: i32) -> i32 {
+        self.list.iter().enumerate().filter(|&v| *v.1 == target).choose(&mut self.rng).unwrap().0 as i32//target必然在list中，直接unwrap
+    }
+}
+
+//use rand::Rng;
+struct Solution_Reservoir_Sampling {
+    list: Vec<i32>,
+    rng: rand::rngs::ThreadRng,
+}
+impl Solution_Reservoir_Sampling {
+    fn new(nums: Vec<i32>) -> Self {
+        Solution_Reservoir_Sampling {
+            list: nums,
+            rng: rand::thread_rng(),
+        }
+    }
+    fn pick(&mut self, target: i32) -> i32 {
+        let mut ret = self.list.iter().position(|&v| v == target).unwrap(); //target一定在数组中，直接unwrap
+        let mut counter = 3;
+        for i in (ret + 1)..self.list.len() {
+            if self.list[i] == target{
+                if self.rng.gen_range(0..counter) == 0{ // P(1/(counter-1))
+                    ret = i;
+                }
+                counter += 1;
+            }
+        }
+        ret as i32
+    }
+}
+
+
+fn main() {
+    let mut test_struct = Solution_Hash::new(vec![1,2,3,3,3]);
+    for i in 0..100{
+        print!("{} ", test_struct.pick(3));
+    }
+    println!();
+}
 
 #[test]
 fn test_solution() {
